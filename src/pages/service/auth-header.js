ed78144dac2111b2dcfb38172bde029b4853bf16
Muzammil Sait <55350436+saitmuza007@users.noreply.github.com>
2023-11-10 02:16:12 +0530

    Update auth-header.js

--- src/pages/service/auth-header.js
+++ src/pages/service/auth-header.js
@@ -1,9 +1,9 @@
-export default function authHeader() {
-    const user = JSON.parse(localStorage.getItem('user'));
+// export default function authHeader() {
+//     const user = JSON.parse(localStorage.getItem('user'));
   
-    if (user && user.accessToken) {
-      return { 'Bearer': user.accessToken };
-    } else {
-      return {};
-    }
-  }
+//     if (user && user.accessToken) {
+//       return { 'Bearer': user.accessToken };
+//     } else {
+//       return {};
+//     }
+//   }
