b7ec22bd0e73b4c5ad52d73c8ed5c11214d7e4ae
Muzammil Sait <55350436+saitmuza007@users.noreply.github.com>
2023-11-10 02:04:09 +0530

    Delete src/pages/schema directory

--- src/pages/schema/login.jsx
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup"
-
-export const loginSchema=Yup.object({
-   
-    email:Yup.string().email().required("Please Enter a Valid Email"),
-    password:Yup.string().min(5).max(14).required("Please Enter a Valid Password"),
-
-})
