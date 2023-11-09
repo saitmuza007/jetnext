f8077c0288c42b959eb94b65c383f5023e5cf6ed
Muzammil Sait <55350436+saitmuza007@users.noreply.github.com>
2023-11-10 02:01:04 +0530

    Update users.js

--- src/pages/users.js
+++ src/pages/users.js
@@ -4,8 +4,7 @@ import Navbar from '@/components/Navbar';
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react';
-import DataTable from "react-data-table-component";
-import Footer from '@/components/footer';
+import Footer from '@/components/Footer';
 ;
 
 const Users=()=>{
